fecd53de66870e111c117e7770fd7b2f73b39c42
Viktor <valkirinn@gmail.com>
2019-11-11 16:31:52 +0200

    Frogger 1.0

--- js/app.js
+++ js/app.js
@@ -1,39 +1,72 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
+    this.x = x;
+	this.y = y;
+	this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x +=  + (this.speed * dt);
+    
+    if (this.x > 550) {
+    	this.x = -100;
+		this.speed = Math.floor(100 + (Math.random() * 200));
+    };
+    
+    if (player.x > this.x - 60 && 
+        player.x < this.x + 60 && 
+        player.y > this.y - 60 &&
+        player.y < this.y + 60) {
+        player.x = 200;
+        player.y = 400;
+	};
+    
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var allEnemies = []
+var enemyLocation = [60, 140, 220]
+
+enemyLocation.forEach(function (y) {
+    allEnemies.push(new Enemy(0, y, 200))
+});
+
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function(x, y){
+	this.x = x;
+	this.y = y;
+	this.sprite = 'images/char-boy.png';
+}
 
+Player.prototype.update = function(){
+	if (this.y < 60) {
+		this.x = 200;
+		this.y = 400;
+	};
+	
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+Player.prototype.handleInput = function(key){
+	if (key === 'left' && this.x > 0) {
+        this.x = this.x - 100;
+    } else if (key === 'right' && this.x != 400) {
+        this.x = this.x + 100;
+    } else if (key === 'up') {
+        this.y = this.y - 83;
+    } else if (key === 'down' && this.y != 400) {
+        this.y = this.y + 83;
+    }
+};
 
+var player = new Player(200, 400);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
